8e8f3e3e00e8a820e6abf602e19e789ed2e42a18
Kevin Dyes <Kevin Dyes>
2016-11-23 13:45:13 +0100

    first commit: added existing VS2015 project

---
+++ ConnectionForInteraction/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "Connect.h"
+
+#define DEFAULT_PORT "27015"
+#define IP_ADRESS "192.168.0.1"
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+
+
+
+	return 0;
+}
